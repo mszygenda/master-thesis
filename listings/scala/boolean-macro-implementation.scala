9340b4e25d39cfaacc8d1be0a28c3238111ddebd
Mateusz Szygenda <mateusz.szygenda@gmail.com>
2014-04-19 13:43:56 +0200

    added details about macro implementation

---
+++ listings/scala/boolean-macro-implementation.scala
@@ -0,0 +1,36 @@
+class BoolExprToConditionMacro[C <: Context](val context: C) extends BooleanExpressionTransformer with ConditionsCodeBuilder {
+  import context._
+
+  override def onComparison(leftOp: Tree, cmpOperator: BinaryComparisonOperator, rightOp: Tree): Tree = {
+    val leftSelector = buildSelector(leftOp)
+    val rightSelector = buildSelector(rightOp)
+
+    buildPropertyComparison(leftSelector, cmpOperator, rightSelector)
+  }
+
+  override def onUnaryLogicalOperation(leftOp: Tree, operator: UnaryLogicalOperator): Tree = {
+    operator match {
+      case Not => {
+        buildNot(leftOp)
+      }
+    }
+  }
+
+  override def onBinaryLogicalOperation(leftOp: Tree, operator: BinaryLogicalOperator, rightOp: Tree): Tree = {
+    operator match {
+      case Or => {
+        buildOr(leftOp, rightOp)
+      }
+      case And => {
+        buildAnd(leftOp, rightOp)
+      }
+    }
+  }
+
+  override def onCustomUnaryOperator(leftOp: Tree, customOp: CustomOperator): Tree = {
+    val propSelector = buildObjectPropertySelector(leftOp, customOp.name)
+    val trueValueSelector = buildSimpleValueSelector(literal(true))
+
+    buildPropertyComparison(propSelector, Eq, trueValueSelector)
+  }
+}

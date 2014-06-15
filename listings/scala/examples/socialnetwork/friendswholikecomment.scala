d10d2bf674eeb2dcb1d900c40cc807cd02d05837
Mateusz Szygenda <mateusz.szygenda@gmail.com>
2014-06-15 16:52:54 +0200

    fixed listing style

--- listings/scala/examples/socialnetwork/friendswholikecomment.scala
+++ listings/scala/examples/socialnetwork/friendswholikecomment.scala
@@ -1,6 +1,6 @@
 def friendsWhoLikeComment(person: Person, content: String) = {
   val (friend, comment) = (p[Person], p[Comment])
-  val q = { person knows { friend likes comment} } where {
+  val q = { person knows { friend likes comment } } where {
             comment.content == content
           } select(friend, comment)
 

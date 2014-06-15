d56f07c90aa3f47a7a7414e1b3faa91414f987ff
Mateusz Szygenda <mateusz.szygenda@gmail.com>
2014-06-15 16:31:11 +0200

    added friendswholikecomment query example

---
+++ listings/scala/examples/socialnetwork/friendswholikecomment-exec.scala
@@ -0,0 +1,4 @@
+// Wyszukanie Johna po identyfikatorze
+val john = Person.find(0).get
+
+Person.friendsWhoLikeComment(john, "Wise Comment")
---
+++ listings/scala/examples/socialnetwork/friendswholikecomment.scala
@@ -0,0 +1,8 @@
+def friendsWhoLikeComment(person: Person, content: String) = {
+  val (friend, comment) = (p[Person], p[Comment])
+  val q = { person knows { friend likes comment} } where {
+            comment.content == content
+          } select(friend, comment)
+
+  q.exec((f, c) => (f, c))
+}
